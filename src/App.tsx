5185b38c346d03130d30328ee9fd539d07434f1f
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-10 19:19:13 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+      <div className="min-h-screen flex justify-center items-center">
+          <h1 className="text-3xl font-bold text-blue-600">
+              Hello world
+          </h1>
+      </div>
+  );
+}
