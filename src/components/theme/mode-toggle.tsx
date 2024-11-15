508ce0da857dbfb1ac6b13e1fa1b1785f937cbda
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-15 10:32:15 -0600

    updated styles to match social icons

--- src/components/theme/mode-toggle.tsx
+++ src/components/theme/mode-toggle.tsx
@@ -1,25 +1,25 @@
-import { Moon, Sun } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
+import { Moon, Sun } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme/theme-provider"
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "@/components/theme/theme-provider";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+        <button
+          aria-label="Toggle theme"
+          className="p-0 bg-transparent border-none outline-none focus:outline-none flex items-center justify-center"
+        >
+          <Sun className="h-6 w-6 transition-all dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-zinc-400" />
+        </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
@@ -33,5 +33,5 @@ export function ModeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
