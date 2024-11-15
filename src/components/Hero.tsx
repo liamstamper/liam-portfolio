2037f8d3780f25e84fec1a2b9289190f7b79368e
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-15 10:18:02 -0600

    new hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,34 +3,35 @@ import LiamImage from "@/assets/Liam.jpeg";
 const Hero = () => {
   return (
     <div className="container dark:text-zinc-50 pt-20 flex flex-col md:flex-row items-center md:items-start justify-between gap-8 pb-4">
-      <div className="text-center md:text-left max-w-lg space-y-4">
+      <div className="text-center md:text-left max-w-lg space-y-4 order-2">
         <h1 className="text-4xl font-bold">Hi, I'm Liam 👋</h1>
 
         <p className="text-md dark:text-white">
-          I'm a Software Developer based in Iowa City who loves the outdoors and
-          exploring new tech.
+          I'm a{" "}
+          <span className="underline font-semibold">full-stack developer</span>{" "}
+          based in Iowa City. I love exploring new tech and getting outdoors.
         </p>
 
         <p className="text-md dark:text-zinc-300">
-          I work primarily with{" "}
-          <span className="font-semibold">React, TypeScript, and Nest.js</span>,
-          and I'm currently developing{" "}
+          I like working with{" "}
+          <span className="font-semibold">React, TypeScript, FastApi and Nest.js</span>.
+          I'm currently working on{" "}
           <a
             href="https://github.com/aaw3/NextFileManager"
-            className="font-semibold text-blue-500"
+            className="font-semibold text-blue-400"
             target="_blank"
           >
             NextFileManager
           </a>
           . Also check out{" "}
-          <a href="dev" className="font-semibold text-blue-500">
+          <a href="dev" className="font-semibold text-blue-400">
             my blog
           </a>
           .
         </p>
       </div>
 
-      <div className="flex-shrink-0 self-center md:self-start">
+      <div className="flex-shrink-0 self-center md:self-start order:1">
         <img
           src={LiamImage}
           alt="Liam"
