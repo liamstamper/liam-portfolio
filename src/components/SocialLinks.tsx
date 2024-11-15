b94c0ec01e755a499be17daecaf713168f9b03dc
Liam Stamper <136619788+liamstamper@users.noreply.github.com>
2024-11-15 10:18:20 -0600

    more unifiorm and added theme controller

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,13 +1,15 @@
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import { ModeToggle } from "./theme/mode-toggle";
 
 const SocialLinks = () => {
   return (
-    <div className="container flex space-x-4">
+    <div className="container flex items-center space-x-4">
       <a
         href="https://linkedin.com/in/liamstamper"
         target="_blank"
         rel="noopener noreferrer"
         aria-label="LinkedIn"
+        className="hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors"
       >
         <FaLinkedin className="w-6 h-6 dark:text-zinc-400" />
       </a>
@@ -16,12 +18,20 @@ const SocialLinks = () => {
         target="_blank"
         rel="noopener noreferrer"
         aria-label="GitHub"
+        className="hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors"
       >
         <FaGithub className="w-6 h-6 dark:text-zinc-400" />
       </a>
-      <a href="mailto:liam.stamper@gmail.com" aria-label="Email">
+      <a
+        href="mailto:liam.stamper@gmail.com"
+        aria-label="Email"
+        className="hover:text-zinc-600 dark:hover:text-zinc-300 transition-colors"
+      >
         <FaEnvelope className="w-6 h-6 dark:text-zinc-400" />
       </a>
+      <div className="ml-4">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
